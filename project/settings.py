c83aac2d7429a9ad4d71b97cf81802971291d9c4
diya <shresthadiya59@gmail.com>
2025-01-25 09:35:41 +0545

    database config

--- project/settings.py
+++ project/settings.py
@@ -79,12 +79,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'restaurant',
+            'USER': 'postgres',
+            'PASSWORD': '123',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- rms/models.py
+++ rms/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+User = get_user_model() 
 
 # Create your models here.
 class Category(models.Model):
@@ -9,18 +9,26 @@ class Category(models.Model):
 
 class Food(models.Model):
 	name = models.CharField(max_length=50)
-	description = models.TextField(null =True,blank= True)
-	category = models.ForeignKey(Category, on_delete = models.CASCADE)
 	price = models.IntegerField()
+	category = models.ForeignKey(Category, on_delete = models.CASCADE)
 
 class Table(models.Model):
    number = models.IntegerField()
    is_available = models.BooleanField(default = False)
 
 class Order(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	table_id = models.ForeignKey(Table, on_delete=models.CASCADE)
-	quantity = models.IntegerField(default = 1)
+   PENDING = 'P'
+   COMPLETED = 'C'
+   STATUS_CHOICE = {
+      (PENDING, 'Pending'),
+      (COMPLETED, 'Completed'),
+   }
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   table = models.ForeignKey(Table, on_delete=models.CASCADE)
+   quantity = models.IntegerField(default=1)
+   total_price = models.FloatField(default=1)
+   status = models.CharField(max_length=1, choices=STATUS_CHOICE, default=PENDING)
+   payment = models.BooleanField(default=False)
 
 class OrderItem(models.Model):
    order = models.ForeignKey(Order, on_delete = models.PROTECT)
