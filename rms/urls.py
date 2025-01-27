d5448e09356fa6c556e50dea1fee1e36d4d20e0a
diya <shresthadiya59@gmail.com>
2025-01-27 17:02:13 +0545

    post delete apis

--- rms/urls.py
+++ rms/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-from .views import category_list
+from .views import *
 urlpatterns = [
    path('category-list',category_list),
+   path('category-detail/<pk>/',category_details),
 ]
--- rms/views.py
+++ rms/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from django.http import HttpResponse
-from .models import Category
+from .models import *
 from .serializers import CategorySerializer
 from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.serializers import ValidationError
+
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def category_list(request):
-   category = Category.objects.all()
-   serializer = CategorySerializer(category,many = True)
-   return Response(serializer.data)
+   if request.method == 'GET':
+      category = Category.objects.all()
+      serializer = CategorySerializer(category,many = True)
+      return Response(serializer.data)
+   else:
+      serializer = CategorySerializer(data = request.data)
+      serializer.is_valid(raise_exception = True)
+      serializer.save()
+      
+      return Response({"details":"Data has been created"},status=status.HTTP_201_CREATED)
+
+@api_view(['GET','DELETE'])
+def category_details(request,pk):
+   category = Category.objects.get(pk = pk)
+   if request.method == "GET":
+      serializer = CategorySerializer(category)
+      return Response(serializer.data)
+   else:
+      items = OrderItem.objects.filter(food__category = category).count()
+      if items > 0:
+         raise ValidationError({"details":"Orderitem related to this category exist."})
+      else:
+         category.delete()
+         return Response({"details":"Category deleted"}, status= status.HTTP_404_NOT_FOUND)
